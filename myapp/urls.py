1321879d728a0aa61be33ada8a154f18be6da584
Yoiner Duran <duranyoiner@gmail.com>
2023-04-05 00:33:51 -0500

    curso terminado

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',index),
-    path('about/',about),
+    path('',index,name='home'),
+    path('about/',about,name='about'),
     path('hello/<str:username>/',hello),
-    path('projects/',projects),
-    path('tasks/',tasks),
+    path('projects/',projects,name='projects'),
+    path('projects/<int:id>/',project_detail,name='project_detail'),
+    path('tasks/',tasks,name='tasks'),
+    path('create_task/',create_task,name='create_task'),
+    path('create_project/',create_project,name='create_project'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.http import HttpResponse,JsonResponse
 from .models import Project,Task
+from .forms import CreateNewTask,CreateNewProject
 
 def index(request):
     title='Django Course!!'
@@ -22,13 +23,44 @@ def projects(request):
     #con .values() es para traer todos los valores y list para poderlo pasar como Json 
     #projects=list(Project.objects.values())
     projects=Project.objects.all()
-    return render(request,'projects.html',{
+    return render(request,'projects/projects.html',{
         'projects':projects
     })
 
 def tasks(request):
     #task=get_object_or_404(Task,title=name)
     tasks=Task.objects.all()
-    return render(request,'tasks.html',{
+    return render(request,'tasks/tasks.html',{
         'tasks':tasks
     })
+
+def create_task(request):
+    if request.method == 'GET':
+      #le muestro la interfaz
+      return render(request,'tasks/create_task.html',{
+        'form':CreateNewTask()
+    })
+    else:
+      #resivo los valores
+      Task.objects.create(title=request.POST['title'],description=request.POST['description'],project_id=2)
+      return redirect('tasks')
+
+def create_project(request):
+    if request.method=='GET':
+      return render(request,'projects/create_project.html',{
+        'form':CreateNewProject()
+    })
+    else:
+       Project.objects.create(name=request.POST['name'])
+       return redirect('projects')
+
+def project_detail(request,id):
+   project = get_object_or_404(Project,id=id)
+   tasks = Task.objects.filter(project_id=id)
+   return render(request,'projects/detail.html',{
+      'project':project,
+      'tasks':tasks
+   })
+
+def task_detail(request):
+   pass
